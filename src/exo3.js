d9c283547a6f50e2e778bc65fbfdc7ef0a7ad2d7
pegCalde <calderonpeg@gmail.com>
2021-01-06 12:03:19 +0100

    testing orientation mobile

--- src/exo3.js
+++ src/exo3.js
@@ -1,12 +1,13 @@
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceOrientation", process, false);
 } else {
-    // Le navigateur ne supporte pas l'événement deviceorientation
+    "<h2>impossible de détecter l'orientation de l'appareil, le navigateur ne supporte pas cet évènement.</h2>";
 }
 
 function process(event) {
     var alpha = event.alpha; //angle de rotation (en degrés) autour de l’axe Z
     var beta = event.beta; //angle d’inclinaison (en degrés) autour de l’axe X
     var gamma = event.gamma; //angle d’inclinaison (en degrés) autour de l’axe Y
+    var info = "Test affichage texte <br>";
     document.getElementById("devOr").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>"; 
 }
