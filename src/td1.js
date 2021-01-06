23817c511eb9a14532465a635662c7bc30cc42b6
pegCalde <calderonpeg@gmail.com>
2021-01-06 10:47:48 +0100

    Update td1.js

--- src/td1.js
+++ src/td1.js
@@ -1,15 +1,3 @@
-// Position par défaut
-var defaultPos = new google.maps.LatLng(48.579400,7.7519);
-// Options de la carte
-var optionsGmaps = {
-    center:defaultPos,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoom: 15
-};
-// Initialisation de la carte pour l'élément portant l'id "map"
-var map = new google.maps.Map(document.getElementById("map"), optionsGmaps);
-var stockLatLong;
-
 function getMyPosition(position) {
     var infoPos = "Position :\n";
     infoPos += "Latitude : " + position.coords.latitude + "\n";
@@ -19,7 +7,7 @@ function getMyPosition(position) {
 }
 
 function watchMyPosition(position) {
-    var infoPos = "Position déterminée :\n";
+    var infoPos = "Position :\n";
     infoPos += "Latitude : "+position.coords.latitude +"\n";
     infoPos += "Longitude: "+position.coords.longitude+"\n";
     infoPos += "Altitude : "+position.coords.altitude +"\n";
@@ -33,5 +21,5 @@ if (navigator.geolocation) {
 
 var watchId = navigator.geolocation.watchPosition(watchMyPosition);
 
-navigator.geolocation.getCurrentPosition(maPosition, erreurPosition,{maximumAge:600000, enableHighAccuracy:true});
+navigator.geolocation.getCurrentPosition(getMyPosition,{maximumAge:600000, enableHighAccuracy:true});
 
