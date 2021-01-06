a366a9ac155109055b01e85aa42dc95e80266726
pegCalde <calderonpeg@gmail.com>
2021-01-06 12:08:16 +0100

    testing show on mobile

--- src/exo3.js
+++ src/exo3.js
@@ -8,6 +8,5 @@ function process(event) {
     var alpha = event.alpha; //angle de rotation (en degrés) autour de l’axe Z
     var beta = event.beta; //angle d’inclinaison (en degrés) autour de l’axe X
     var gamma = event.gamma; //angle d’inclinaison (en degrés) autour de l’axe Y
-    var info = "Test affichage texte <br>";
-    document.getElementById("devOr").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>"; 
+    document.getElementById("devOr").innerHTML = "Alpha : " + alpha + "</br>Beta : " + beta + "</br>Gamma : " + gamma; 
 }
