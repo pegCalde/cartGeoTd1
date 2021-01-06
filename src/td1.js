17e640c5d6f0639dc471e636b1c488caa5b6cb37
pegCalde <calderonpeg@gmail.com>
2021-01-06 11:24:17 +0100

    timestamp

--- src/td1.js
+++ src/td1.js
@@ -1,24 +1,34 @@
+//timestamp
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
 function getMyPosition(position) {
     var infoPos = "Position actuelle: <br>";
     infoPos += "Latitude: " + position.coords.latitude + "<br>";
     infoPos += "Longitude: " + position.coords.longitude + "<br>";
     infoPos += "Altitude: " + position.coords.altitude + "<br>";
-    infoPos += "Vitesse : "+position.coords.speed +"<br>";
+    infoPos += "Vitesse : "+ position.coords.speed + "<br>";
+    infoPos += "Date: " + new Date(position.timestamp);
     document.getElementById("infoPos").innerHTML = infoPos;
 }
 
 function watchMyPosition(position) {
     var infoPos = "Position actuelle: <br>";
-    infoPos += "Latitude: "+position.coords.latitude +"<br>";
-    infoPos += "Longitude: "+position.coords.longitude+"<br>";
-    infoPos += "Altitude: "+position.coords.altitude +"<br>";
-    infoPos += "Vitesse : "+position.coords.speed +"<br>";
+    infoPos += "Latitude: " + position.coords.latitude + "<br>";
+    infoPos += "Longitude: " + position.coords.longitude + "<br>";
+    infoPos += "Altitude: " + position.coords.altitude + "<br>";
+    infoPos += "Vitesse : " + position.coords.speed + "<br>";
     document.getElementById("infoPos").innerHTML = infoPos;
 }
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getMyPosition);
+    navigator.geolocation.getCurrentPosition(getMyPosition, options);
+} else {
+    "<h2>La géolocalisation n'est pas possible</h2>";
 }
 
-var watchId = navigator.geolocation.watchPosition(watchMyPosition);
+var watchId = navigator.geolocation.watchPosition(watchMyPosition, options);
 navigator.geolocation.clearWatch(watchId);
