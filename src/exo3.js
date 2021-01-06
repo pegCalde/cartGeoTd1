4c0dadcd1bf5255ba4dc1f8de4c2f4852e0566bb
pegCalde <calderonpeg@gmail.com>
2021-01-06 12:20:05 +0100

    testing show everything

--- src/exo3.js
+++ src/exo3.js
@@ -4,12 +4,12 @@ if (window.DeviceOrientationEvent) {
     "<h2>impossible de détecter l'orientation de l'appareil, le navigateur ne supporte pas cet évènement.</h2>";
 }
 
-function processOrientation(event) {
+/*function process(event) {
     var alpha = event.alpha; //angle de rotation (en degrés) autour de l’axe Z
     var beta = event.beta; //angle d’inclinaison (en degrés) autour de l’axe X
     var gamma = event.gamma; //angle d’inclinaison (en degrés) autour de l’axe Y
     document.getElementById("devOr").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>";
-}
+}*/
 
 //L'API Device Motion ne retourne pas de valeurs d'angles mais d'accélération en mètres par seconde au carré (m/s²), sur les trois axes x, y et z. 
 //Son usage est semblable celui de l'Orientation.
@@ -19,9 +19,13 @@ if (window.DeviceMotionEvent) {
     "<h2>impossible de détecter le mouvement de l'appareil, le navigateur ne supporte pas cet évènement.</h2>";
 }
 
-function processMotion(event) {
+function process(event) {
+    var alpha = event.alpha; //angle de rotation (en degrés) autour de l’axe Z
+    var beta = event.beta; //angle d’inclinaison (en degrés) autour de l’axe X
+    var gamma = event.gamma; //angle d’inclinaison (en degrés) autour de l’axe Y
     var x = event.accelerationIncludingGravity.x;
     var y = event.accelerationIncludingGravity.y;
     var z = event.accelerationIncludingGravity.z;
+    document.getElementById("devOr").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>";
     document.getElementById("devMotion").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>";
 }
